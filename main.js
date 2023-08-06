8a90b386f167fdacda79b2ba6fb7f31b116766ef
thompskr51 <141582042+thompskr51@users.noreply.github.com>
2023-08-06 15:11:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+// number of years I've been alive
+const myAge = 42
+// years 1 & 2 in dog years
+var earlyYears = 2
+earlyYears = earlyYears *= 10.5
+// number of years I've been alive less years 1 & 2
+var laterYears = myAge - 2
+// years 3 onward in dog years
+laterYears = laterYears *= 4
+// my total age in dog years
+var myAgeInDogYears = earlyYears + laterYears
+// making my name have only lowercase letters
+var myName = ('Kristen'.toLowerCase()); 
+// writing a complete sentence and inserting the variable info
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); 
